0ef5170ce08385d080b05e2507d4c71a5c28f74b
ユウ <222monndo@gmail.com>
2017-12-27 11:03:45 +0900

    課題　標準クラス7　文字の入れ替え　提出します

---
+++ CampChallenge_java/015.標準クラス/charastrings3.java
@@ -0,0 +1,11 @@
+package charastrings;
+
+public class charastrings3 {
+    public static void main(String[] args){
+           String str = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+           str = str.replace("I","い");
+           str = str.replace("U", "う");
+           System.out.println(str);
+    }
+ 
+}
