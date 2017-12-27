b1aa01bdeceaa5de746ea7a7fbdf2daef27f8fd2
ユウ <222monndo@gmail.com>
2017-12-27 10:39:57 +0900

    課題　標準クラス5　バイト数と文字数の取得　提出します

---
+++ CampChallenge_java/015.標準クラス/charastrings1.java
@@ -0,0 +1,10 @@
+package charastrings;
+
+public class charastrings1 {
+    public static void main(String[] args){
+           String name = "武井祐也";
+           System.out.println(name.getBytes().length);
+           System.out.println(name.length());
+    }
+ 
+}
